1315aa89d4ce18f4987a6ef01b02ccb1510ab055
prismdata-dc <prismdata@add4s.com>
2020-11-07 16:54:02 +0900

    readthedocs

--- _build/html/_sources/index.rst.txt
+++ _build/html/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. whaleshark documentation master file, created by
-   sphinx-quickstart on Sat Nov  7 16:22:33 2020.
+   sphinx-quickstart on Sat Nov  7 16:51:10 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- conf.py
+++ conf.py
@@ -41,12 +41,12 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'kor'
+language = 'ko'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
